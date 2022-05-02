d5669ab532f235e0357cfb983f29919ca9fed9f0
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-02 10:28:22 -0300

    Create TelaHospedagem

---
+++ Components/TelaHospedagem/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function TelaHospedagem() {
+  return (
+    <View>
+      <Text>Restaurantes</Text>
+    </View>
+  );
+}
