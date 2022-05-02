2677a24cced5f0a2372dab2758dd7411382353f3
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-01 21:25:49 -0300

    Create TelaIncial Style

---
+++ Components/TelaInicial/styles.js
@@ -0,0 +1,68 @@
+import { StyleSheet } from 'react-native';
+ const styles = StyleSheet.create({
+    container:{
+
+        borderWidth: 0.5,
+        borderRadius:5,
+        alignItems: 'center',
+        marginTop:50,
+        marginHorizontal:40,
+        justifyContent: 'center',
+        
+
+    
+    },
+    titulo: {
+        marginTop:10,
+        fontWeight:'bold',
+        fontSize:23,
+    },
+    subtitulo:{
+        marginTop:20,
+        fontWeight: '600'
+
+    },
+    img:{
+        marginTop: 20,
+        width: 255,
+        height:150,
+        marginBottom: 15,
+      
+
+    },
+    
+    botao:{ 
+
+        backgroundColor: "#382d00",
+       
+        marginBottom: 15,
+
+
+   
+        
+    },
+
+    buttonrestaurantestextstyle:{
+        color: 'white',
+        paddingVertical:6,
+        paddingHorizontal: 62,
+    
+    },
+
+    buttonpasseiostextstyle:{
+        color: 'white',
+        paddingVertical:6,
+        paddingHorizontal: 82,
+    
+    },
+
+    buttonhospedagemtextstyle:{
+        color: 'white',
+        paddingVertical:6,
+        paddingHorizontal: 68,
+    
+    },
+
+ }) 
+ 
+ export default styles
