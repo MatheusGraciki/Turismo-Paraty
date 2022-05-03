9c6f89cb713b65230e40bcd6d3a90d70864b3d01
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-03 02:07:15 -0300

    Update TelaRestaurante

--- Components/TelaRestaurantes/index.js
+++ Components/TelaRestaurantes/index.js
@@ -4,41 +4,42 @@ import styles from './styles';
 import Img1 from '../../assets/restaurantes/restaurante01.png'
 import Img2 from '../../assets/restaurantes/restaurante02.png'
 import Img3 from '../../assets/restaurantes/restaurante03.png'
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView } from 'react-native-gesture-handler';
+
+
+import { translate } from '../Translate/index';
 
 
 export default function TelaRestaurante() {
   return (
-  <ScrollView>
+  
   
     <View style={styles.container}>
      
       <Text style={styles.titulo}>
-        Bares e Restaurantes
+        {translate('titleRestaurante')}
       </Text>
       <Text style={styles.descricao}>
-      O prazer da boa comida você encontra em Paraty. Com inúmeras opções de restaurantes, a cidade oferece o melhor da culinária internacional Confira alguns bares e restaurantes famosos.
+      {translate('descricaoRestaurante')}
       </Text>
 
         <View style={styles.card}>
           <Image source={Img1} style={styles.cardImg} />
-          <Text style={styles.tituloCard}> ARMAZÉN MAR</Text>
-          <Text> Localização: Rod Rio-Santos</Text>
+          <Text style={styles.tituloCard}> {translate('tituloRestaurante1')}</Text>
+          <Text> {translate('Localizacao1')}</Text>
         </View>
 
       
 
         <View style={styles.card}>
           <Image source={Img2} style={styles.cardImg} />
-          <Text style={styles.tituloCard}> BENDITA'S RESTAURANTE</Text>
-          <Text> Localização: Centro Histórico</Text>
+          <Text style={styles.tituloCard}> {translate('tituloRestaurante2')}</Text>
+          <Text> {translate('Localizacao2')}</Text>
         </View>
 
         <View style={styles.card} >
           <Image source={Img3} style={styles.cardImg} />
-          <Text style={styles.tituloCard}> SEREIA DO MAR PIZZA-BAR</Text>
-          <Text> Localização: Praia do Jabaquara</Text>
+          <Text style={styles.tituloCard}> {translate('tituloRestaurante3')}</Text>
+          <Text> {translate('Localizacao3')}</Text>
         </View>
 
         
@@ -49,7 +50,7 @@ export default function TelaRestaurante() {
         
         
     </View>
-  </ScrollView>
+  
     
   );
 }
