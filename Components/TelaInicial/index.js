196e8500042cc1930b2f6535ddc9421a7421131e
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-03 02:05:41 -0300

    Implement translation

--- Components/TelaInicial/index.js
+++ Components/TelaInicial/index.js
@@ -1,62 +1,146 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Image, Button, TouchableOpacity } from 'react-native'
+import { BottomSheet, ListItem } from 'react-native-elements';
+import { translate } from '../Translate/index.js'
+import logo from '../../assets/Logo.png'
 import FotoParaty from  '../../assets/01.png'
 import styles from './styles'
+import ChangeLanguage from '../Translate/index.js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
 
 
 export default function TelaInicial(props){
+    
+
+    const [isVisible, setIsVisible] = useState(false);
+    const [language, setLanguage] = useState('');
+
+    const list = [
+        {
+            title: 'portugues',
+            onPress: () => setLanguage('br')
+        },
+        {
+            title: 'ingles',
+            onPress: () => setLanguage('en')
+        },
+        {
+            title: 'fechar',
+            containerStyle: { backgroundColor: '#0071bc' },
+            titleStyle: { color: 'white' },
+            onPress: () => setIsVisible(false),
+        },
+    ];
+
+    if (language!='') {
+
+        ChangeLanguage(language)
+    }
+
+    async function checkLang() {
+        const lang = await AsyncStorage.getItem('Translate');
+    
+        if(lang){
+            ChangeLanguage(lang);
+        }
+    }
+      
+    useEffect(() => {
+        checkLang();
+    }, []);
+
+
     return(
-       
-        < View style={styles.container}>
-            <Text style={styles.titulo}> Paraty </Text>
-            <Text style={styles.subtitulo}> Saiba o que visitar em paraty. </Text>
-            <Image style={styles.img} source={FotoParaty}/>
+        <>
 
-            <TouchableOpacity
-                style={styles.botao} 
-                activeOpacity={0.5} 
-                onPress={() => props.navigation.navigate("Restaurantes")}
-            > 
-                
-               
-                
-                <Text style={styles.buttonrestaurantestextstyle}> 
-                    VER RESTAURANTES
-                </Text> 
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.botao} 
+                    activeOpacity={0.5} 
+                    onPress={() => setIsVisible(true)}
 
-            <TouchableOpacity
-                style={styles.botao} 
-                activeOpacity={0.5}
-                onPress={() => props.navigation.navigate("Passeios")}
-            > 
+                >
+                    <Image style={styles.imgbutton} source={logo}/>
+
+                    
+                
+                </TouchableOpacity> 
+                    
+            <BottomSheet modalProps={{}} isVisible={isVisible} containerStyle={{ backgroundColor: 'blur' }}>
+                {list.map((l, i) => (
+                    <ListItem
+                        key={i}
+                        containerStyle={l.containerStyle}
+                        onPress={l.onPress}
+                    >
+                        <ListItem.Content>
+                            <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
+            </BottomSheet>
+
             
-               
-                
-                <Text style={styles.buttonpasseiostextstyle}> 
-                    VER PASSEIOS
-                </Text> 
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                style={styles.botao} 
-                activeOpacity={0.5}
-                onPress={() => props.navigation.navigate("Hospedagem")}
-            > 
-                
-               
-               
-                <Text style={styles.buttonhospedagemtextstyle}> 
-                    VER HOSPEDAGEM
-                </Text> 
-            </TouchableOpacity>
-                  
+                    
             
+            < View style={styles.container}>
+                <Text style={styles.titulo}> 
+                    {translate('title')}</Text>
+                <Text style={styles.subtitulo}>  
+                    {translate('subtitle')} </Text>
+                <Image style={styles.img} source={FotoParaty}/>
+                
+               
+
+                <TouchableOpacity
+                    style={styles.buttons} 
+                    activeOpacity={0.5} 
+                    onPress={() => props.navigation.navigate("Restaurantes")}
+                > 
+                    
+                
+                    
+                    <Text style={styles.buttontext}> 
+                        {translate('buttonr')}
+                    </Text> 
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttons} 
+                    activeOpacity={0.5}
+                    onPress={() => props.navigation.navigate("Passeios")}
+                > 
+                
+                
+                    
+                    <Text style={styles.buttontext}> 
+                        {translate('buttonp')}
+                    </Text> 
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.buttons} 
+                    activeOpacity={0.5}
+                    onPress={() => props.navigation.navigate("Hospedagem")}
+                > 
+                    
+                
+                
+                    <Text style={styles.buttontext}>  
+                        {translate('buttonh')}
+                    </Text> 
+                </TouchableOpacity>
+
+               
+                
+            
+                
+
+            </View>
         
+        </>
             
-
-        </View>
-       
-        
     )
 }
