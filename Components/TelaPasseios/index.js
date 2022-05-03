30371f2664971f6f0325bb6b98f9016d7a451948
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-03 02:06:28 -0300

    Update TelaPasseios

--- Components/TelaPasseios/index.js
+++ Components/TelaPasseios/index.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styles from './styles';
+import Img1 from '../../assets/passeios/passeio01.png'
+import Img2 from '../../assets/passeios/passeio02.png'
+import Img3 from '../../assets/passeios/passeio03.png'
+import { translate } from '../Translate/index';
+
+
+
+
 
 export default function TelaPasseios() {
   return (
-    <View>
-      <Text>Passeios</Text>
+  
+  
+    <View style={styles.container}>
+     
+      <Text style={styles.titulo}>
+        {translate('titlePasseios')}
+      </Text>
+      <Text style={styles.descricao}>
+        {translate('descricaoPasseios')}
+      </Text>
+
+        <View style={styles.card}>
+          <Text style={styles.tituloCard}> {translate('cardTitle1')} </Text>
+          <Image source={Img1} style={styles.cardImg} />
+          
+        </View>
+
+      
+
+        <View style={styles.card}>
+          <Text style={styles.tituloCard}> {translate('cardTitle2')}</Text>
+          <Image source={Img2} style={styles.cardImg} />
+         
+        </View>
+
+        <View style={styles.card} >
+          <Text style={styles.tituloCard}>{translate('cardTitle3')}</Text>
+          <Image source={Img3} style={styles.cardImg} />
+         
+        </View>
+
+        
+
+        
+
+
+        
+        
     </View>
+
+    
   );
 }
